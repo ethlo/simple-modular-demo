3b174e8727ca6cd77d43d2fccc997fc8b972752d
Morten Haraldsen <dev@ethlo.com>
2025-02-14 13:34:45 +0100

    Cleanup

--- src/main/java/com/example/modularproject/Main.java
+++ src/main/java/com/example/modularproject/Main.java
@@ -1,18 +1,42 @@
 package com.example.modularproject;
 
+import com.ethlo.chronograph.Chronograph;
+import com.ethlo.chronograph.OutputConfig;
+import com.ethlo.chronograph.output.table.TableOutputFormatter;
+import com.ethlo.chronograph.output.table.TableThemes;
 import com.ethlo.time.DateTime;
 import com.ethlo.time.DateTimeParser;
 import com.ethlo.time.DateTimeTokens;
 import com.ethlo.time.Field;
 import com.ethlo.time.token.ConfigurableDateTimeParser;
 import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012, builder ->
+                builder.schemaMappers(schemaMappers -> schemaMappers.mapPrefix("https://www.example.org/", "classpath:schema/"))
+        );
+
+        final Chronograph chronograph = Chronograph.create();
+
+        final int runs = 100_000;
         final DateTimeParser yearParser = ConfigurableDateTimeParser.of(DateTimeTokens.digits(Field.YEAR, 4));
-        final DateTime result = yearParser.parse("1999");
-        System.out.println(result.getMostGranularField());
-        final var whatever = new JsonSchemaFactory.Builder().build();
+        final List<DateTime> results = new ArrayList<>(runs);
+        IntStream.rangeClosed(1, runs).forEach(i ->
+                chronograph.time("parse", () ->
+                {
+                    final DateTime result = yearParser.parse("1999");
+                    results.add(result);
+                }));
+
+        System.out.println(results.size());
+        System.out.println(new TableOutputFormatter(TableThemes.OCEAN_BREEZE, OutputConfig.EXTENDED).format(chronograph.getTaskData()));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.example.modularproject {
     requires com.networknt.schema;
     requires com.ethlo.time;
+    requires com.ethlo.chronograph;
 }
